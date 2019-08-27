3fe62a2cb2e661fe2ea573792adb7e1bdabf00b5
yanyupingsheng395 <395351080@qq.com>
2019-08-27 22:40:35 +0800

    增加microservicecloud-provider-dept-8002这个子工程,测试负载均衡,仿照8001工程,主要修改主启动类和yml配置文件,其他和8001工程一样

---
+++ microservicecloud-provider-dept-8002/src/main/java/com/atguigu/springcloud/DeptProvider8002_App.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient	//本服务启动后,自动注册到eureka注册中心中
+@EnableDiscoveryClient   //暴露服务,是别的服务发现
+public class DeptProvider8002_App
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DeptProvider8002_App.class, args);
+	}
+}
